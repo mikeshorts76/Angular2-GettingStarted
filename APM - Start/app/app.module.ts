62bb8df70316f699ecbed64254f10bf165a40a98
Michael <michael.schwartz76@gmail.com>
2017-02-23 07:25:35 -0700

    added a custom filter pipe

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ProductFilterPipe } from './products/product-filter.pipe';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -11,7 +12,8 @@ import { FormsModule } from '@angular/forms';
   ],
   declarations:[
     AppComponent, 
-    ProductListComponent    
+    ProductListComponent,
+    ProductFilterPipe    
   ],
   bootstrap: [ AppComponent ]
 })
